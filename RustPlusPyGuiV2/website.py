6a3bcfd87e00ebbf8626235c7c0d81d1b07f84e0
miki <84585119+miko1034@users.noreply.github.com>
2024-12-07 12:17:55 +0000

    started a couple routes

--- RustPlusPyGuiV2/website.py
+++ RustPlusPyGuiV2/website.py
@@ -1,19 +1,28 @@
-from flask import Flask, render_template, redirect, session, request, abort
+from flask import Flask, render_template, redirect, session, request, abort, redirect
 
 
 
 app = Flask(__name__)
+app.secret_key = "very_secret_key_that_should_be_changed_as_soon_as_possible_please_dear_user"
 
 
 
 # handles the homepage. if user is authed, go straight to dashboard. if not, enter the base code that was set in the cfg.
 @app.route("/",methods=["GET", "POST"])
 def auth():
+#    try:
+#        session["authed"]
+#    except KeyError:
+#        session["authed"] = False
+#
     if request.method == "GET":
-        if session["authed"] and "authed" in session:
-            pass
-        pass
+        return render_template("auth.html")
     elif request.method == "POST":
         pass
     else:
         abort(404)
+
+
+@app.route("/dashboard", methods=["GET"])
+def dashboard():
+    pass
